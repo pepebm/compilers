df70e32bc859d38eaded7a6acef4473e5a2c1079
José Manuel <jose@thegurucompany.com>
2018-09-06 10:44:43 -0500

    Add comments

--- lexicalAnalyzaer/lexer.py
+++ lexicalAnalyzaer/lexer.py
@@ -14,15 +14,21 @@ def globales(prog, pos, long):
     progLong = long
 
 def getToken(imprime = True):
+    # Checks if lexer has data
     if lexer.lexdata == None: 
         lexer.input(programa)
+    # Get token
     t = lexer.token()
+    # Something went wrong reading the file
     if not t:
         print('Error getting the next char')
         return
     else:
+        # Checks if the next token is end of file ($), this is compared
+        # with globalTypes Enum
         if t.value == TokenType.ENDFILE.value: 
-            return TokenType.ENDFILE, TokenType.ENDFILE.value 
+            return TokenType.ENDFILE, TokenType.ENDFILE.value
+        # Print every token identified by the lexer 
         if imprime: 
             print(str(t.type) + ' = ' + str(t.value))
         return t.type, t.value
