cfa855849a9d76e31e3accf369171dc5cab515a0
José Manuel <jose@thegurucompany.com>
2018-10-08 20:07:15 -0500

    Parser first commit 🐜

---
+++ parser/parser.py
@@ -0,0 +1,34 @@
+from lex import lexer
+from globalTypes import *
+# @Params
+# prog: que contiene el programa como un string
+# pos: que contiene la posición inicial de la búsqueda (0)
+# long: que contiene la longitud del programa
+def globales(prog, pos, long):
+    global programa 
+    global posicion
+    global progLong
+    # Lexer will save the position and total length of the program
+    programa = prog
+    posicion = pos
+    progLong = long
+
+def parser(imprime = True):
+    # Checks if lexer has data
+    if lexer.lexdata == None: 
+        lexer.input(programa)
+    # Get token
+    t = lexer.token()
+    # Something went wrong reading the file
+    if not t:
+        print('Error getting the next char')
+        return
+    else:
+        # Checks if the next token is end of file ($), this is compared
+        # with globalTypes Enum
+        if t.value == TokenType.ENDFILE.value: 
+            return TokenType.ENDFILE, TokenType.ENDFILE.value
+        # Print every token identified by the lexer 
+        if imprime: 
+            print(str(t.type) + ' = ' + str(t.value))
+        return t.type, t.value
---
+++ parser/test.py
@@ -0,0 +1,273 @@
+# Yacc example
+import ply.yacc as yacc
+import sys
+from lex import tokens
+# Get the token map from the lexer.  This is required.
+import lex
+import pry
+
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = p[1] + p[3]
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = p[1] - p[3]
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = p[1] * p[3]
+
+def p_term_div(p):
+    'term : term DIVIDE factor'
+    p[0] = p[1] / p[3]
+
+def p_program(p):
+	'program : declaration_list'
+	pass
+
+def p_declaration_list_1(p):
+	'declaration_list : declaration_list declaration'
+	 #p[0] = p[1] + p[2]  
+	pass
+
+def p_declaration_list_2(p):
+	'declaration_list : declaration'
+	pass
+
+def p_declaration(p):
+	'''declaration : var_declaration
+				  | fun_declaration'''
+	pass
+
+def p_var_declaration_1(p):
+	'var_declaration : type_specifier ID SEMICOLON'
+	pass
+
+def p_var_declaration_2(p):
+	'var_declaration : type_specifier ID LBRACKET NUMBER RBRACKET SEMICOLON'
+	pass
+
+def p_fun_declaration(p):
+	'fun_declaration : type_specifier ID LPAREN params RPAREN compount_stmt'
+	pass
+
+def p_type_specifier_1(p):
+	'type_specifier : INT'
+	pass
+
+def p_type_specifier_2(p):
+	'type_specifier : VOID'
+	pass
+
+def p_params_1(p):
+	'params : param_list'
+	pass
+
+def p_params_2(p):
+	'params : VOID'
+	pass
+
+def p_param_list_1(p):
+	'param_list : param_list COMMA param'
+	pass
+
+def p_param_list_2(p):
+	'param_list : param'
+	pass
+
+def p_param_list_3(p):
+	'param_list : empty'
+	pass
+
+def p_param_1(p):
+	'param : type_specifier ID'
+	pass
+
+def p_param_2(p):
+	'param : type_specifier ID LBRACKET RBRACKET'
+	pass
+
+def p_compount_stmt(p):
+	'compount_stmt : LBLOCK local_declarations statement_list RBLOCK'
+	pass
+
+def p_local_declarations_1(p):
+	'local_declarations : local_declarations var_declaration'
+	pass
+
+def p_local_declarations_2(p):
+	'local_declarations : empty'
+	pass
+
+def p_statement_list_1(p):
+	'statement_list : statement_list statement'
+	pass
+
+def p_statement_list_2(p):
+	'statement_list : empty'	
+	pass
+
+def p_statement(p):
+	'''statement : expression_stmt
+				| compount_stmt
+				| selection_stmt
+				| iteration_stmt
+				| return_stmt
+	'''
+	pass
+
+def p_expression_stmt_1(p):
+	'expression_stmt : expression SEMICOLON'
+	pass
+
+def p_expression_stmt_2(p):
+	'expression_stmt : SEMICOLON'
+	pass
+
+def p_selection_stmt_1(p):
+	'selection_stmt : IF LPAREN expression RPAREN statement'
+	pass
+
+def p_selection_stmt_2(p):
+	'selection_stmt : IF LPAREN expression RPAREN statement ELSE statement'
+	pass
+
+def p_iteration_stmt(p):
+	'iteration_stmt : WHILE LPAREN expression RPAREN statement'
+	pass
+
+def p_return_stmt_1(p):
+	'return_stmt : RETURN SEMICOLON'
+	pass
+
+def p_return_stmt_2(p):
+	'return_stmt : RETURN expression SEMICOLON'
+	pass
+
+def p_expression_1(p):
+	'expression : var EQUAL expression'
+	pass
+
+def p_expression_2(p):
+	'expression : simple_expression'
+	pass
+
+def p_var_1(p):
+	'var : ID'
+	pass
+
+def p_var_2(p):
+	'var : ID LBRACKET expression RBRACKET'
+	pass
+
+def p_simple_expression_1(p):
+	'simple_expression : additive_expression relop additive_expression'
+	pass
+
+def p_simple_expression_2(p):
+	'simple_expression : additive_expression'
+	pass
+
+
+def p_relop(p):
+	'''relop : LESS 
+			| LESSEQUAL
+			| GREATER
+			| GREATEREQUAL
+			| DEQUAL
+			| DISTINT
+	'''
+	pass
+
+def p_additive_expression_1(p):
+	'additive_expression : additive_expression addop term'
+	pass
+
+def p_additive_expression_2(p):
+	'additive_expression : term'
+	pass
+
+def p_addop(p):
+	'''addop : PLUS 
+			| MINUS
+	'''
+	pass
+
+def p_term_1(p):
+	'term : term mulop factor'
+	pass
+
+def p_term_2(p):
+	'term : factor'
+	pass
+
+def p_mulop(p):
+	'''mulop : 	TIMES
+				| DIVIDE
+	'''
+	pass
+
+def p_factor_1(p):
+	'factor : LPAREN expression RPAREN'
+	pass
+
+def p_factor_2(p):
+	'factor : var'
+	pass
+
+def p_factor_3(p):
+	'factor : call'
+	pass
+
+def p_factor_4(p):
+	'factor : NUMBER' 
+	pass
+
+def p_call(p):
+	'call : ID LPAREN args RPAREN'
+	pass
+
+def p_args(p):
+	'''args : args_list
+			| empty
+	'''
+	pass
+
+def p_args_list_1(p):
+	'args_list : args_list COMMA expression'
+	pass
+
+def p_args_list_2(p):
+	'args_list : expression'
+	pass
+
+def p_empty(p):
+	'empty :'
+	pass
+
+def p_error(p):
+    if p is not None:
+        print("Syntax error at line " + str(p.lexer.lineno) + " Unexpected token  " + str(p.value))
+    else:
+        print("Syntax error at line: " + str(cminus_lexer.lexer.lineno))
+		
+
+
+# Build the parser
+parser = yacc.yacc()
+
+if (len(sys.argv) > 1):
+		fin = sys.argv[1]
+else:
+    fin = 'sample.c-'
+
+f = open(fin, 'r')
+data = f.read()
+data = data + '$'
+print(data)
+parser.parse(data, tracking=True)
